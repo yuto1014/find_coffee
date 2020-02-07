69f6cffe7fa656b91c1570e4dbd4afc633644573
yuto1014 <yto.oct.14@gmail.com>
2020-02-07 04:56:34 +0000

    [Update]投稿一覧ページ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,122 +26,113 @@
 $(document).on("turbolinks:load", function() {
 	//ヘッダーナブバー右
   //nav_tagホバー時に発火(nav_tag_textの表示)
-  $('.nav_tag1').hover(
+  $('.nav_tag_mypage').hover(
     function(){
-      $('.nav_tag_text1').fadeIn();
+      $('.nav_tag_text_mypage').fadeIn();
     },
     function(){
-      $('.nav_tag_text1').fadeOut();
+      $('.nav_tag_text_mypage').fadeOut();
     }
     );
 
-  $('.nav_tag2').hover(
+  $('.nav_tag_find').hover(
     function(){
-      $('.nav_tag_text2').fadeIn();
+      $('.nav_tag_text_find').fadeIn();
     },
     function(){
-      $('.nav_tag_text2').fadeOut();
+      $('.nav_tag_text_find').fadeOut();
     }
     );
 
-  $('.nav_tag3').hover(
+  $('.nav_tag_index').hover(
     function(){
-      $('.nav_tag_text3').fadeIn();
+      $('.nav_tag_text_index').fadeIn();
     },
     function(){
-      $('.nav_tag_text3').fadeOut();
+      $('.nav_tag_text_index').fadeOut();
     }
     );
 
-  $('.nav_tag4').hover(
+  $('.nav_tag_signout').hover(
     function(){
-      $('.nav_tag_text4').fadeIn();
+      $('.nav_tag_text_signout').fadeIn();
     },
     function(){
-      $('.nav_tag_text4').fadeOut();
+      $('.nav_tag_text_signout').fadeOut();
     }
     );
 
-  $('.nav_tag5').hover(
+  $('.nav_tag_timeline').hover(
     function(){
-      $('.nav_tag_text5').fadeIn();
+      $('.nav_tag_text_timeline').fadeIn();
     },
     function(){
-      $('.nav_tag_text5').fadeOut();
+      $('.nav_tag_text_timeline').fadeOut();
     }
     );
 
-  $('.nav_tag6').hover(
+  $('.nav_tag_signup').hover(
     function(){
-      $('.nav_tag_text6').fadeIn();
+      $('.nav_tag_text_signup').fadeIn();
     },
     function(){
-      $('.nav_tag_text6').fadeOut();
+      $('.nav_tag_text_signup').fadeOut();
     }
     );
 
-  $('.nav_tag7').hover(
+  $('.nav_tag_signin').hover(
     function(){
-      $('.nav_tag_text7').fadeIn();
+      $('.nav_tag_text_signin').fadeIn();
     },
     function(){
-      $('.nav_tag_text7').fadeOut();
+      $('.nav_tag_text_signin').fadeOut();
     }
     );
 
-  $('.nav_tag8').hover(
+  $('.nav_tag_dm').hover(
     function(){
-      $('.nav_tag_text8').fadeIn();
+      $('.nav_tag_text_dm').fadeIn();
     },
     function(){
-      $('.nav_tag_text8').fadeOut();
+      $('.nav_tag_text_dm').fadeOut();
     }
     );
 
-  $('.nav_tag9').hover(
+  $('.nav_tag_notice').hover(
     function(){
-      $('.nav_tag_text9').fadeIn();
+      $('.nav_tag_text_notice').fadeIn();
     },
     function(){
-      $('.nav_tag_text9').fadeOut();
-    }
-    );
-
-  $('.nav_tag10').hover(
-    function(){
-      $('.nav_tag_text10').fadeIn();
-    },
-    function(){
-      $('.nav_tag_text10').fadeOut();
+      $('.nav_tag_text_notice').fadeOut();
     }
     );
 
   //トップページFunction(機能説明)-----------------------------------
   //アイコンホバー時に発火(addClass)
-  $('.information_list1').hover(
+  $('.information_list_recommend').hover(
     function() {
-      $(this).find('.information_none1').addClass('text-active');
+      $(this).find('.information_list_recommend_none').addClass('text-active');
     },
     function() {
-         $(this).find('.information_none1').removeClass('text-active');
+         $(this).find('.information_list_recommend_none').removeClass('text-active');
     }
   );
 
-  $('.information_list2').hover(
+  $('.information_list_calendar').hover(
     function() {
-      $(this).find('.information_none2').addClass('text-active');
+      $(this).find('.information_list_calendar_none').addClass('text-active');
     },
     function() {
-         $(this).find('.information_none2').removeClass('text-active');
+         $(this).find('.information_list_calendar_none').removeClass('text-active');
     }
     );
 
-  $('.information_list3').hover(
+  $('.information_list_dm').hover(
     function() {
-      $(this).find('.information_none3').addClass('text-active');
+      $(this).find('.information_list_dm_none').addClass('text-active');
     },
     function() {
-         $(this).find('.information_none3').removeClass('text-active');
+         $(this).find('.information_list_dm_none').removeClass('text-active');
     }
     );
 
@@ -174,12 +165,8 @@ $(document).on("turbolinks:load", function() {
       var cy = $(this).height() / 2;
       var dx = e.clientX - cx;
       var dy = e.clientY - cy;
-      $('#mouse_box1').css('left', cx + dx * 1.0);
-      // $('#mouse_box2').css('left', cx + dx * 1.3);
-      // $('#mouse_box3').css('left', cx + dx * 1.5);
-      $('#mouse_box1').css('top', cy + dy * 1.0);
-      // $('#mouse_box2').css('top', cy + dy * 1.1);
-      // $('#mouse_box3').css('top', cy + dy * 1.3);
+      $('#mouse_box').css('left', cx + dx * 1.0);
+      $('#mouse_box').css('top', cy + dy * 1.0);
     });
 
   //検索フォーム-----------------------------------------
